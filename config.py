670c9c0c4c065baaf73d7b11e50dbf52c621e7fd
YOrHaS <100461126+YOrHaS@users.noreply.github.com>
2022-12-12 19:46:24 -0600

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ class Config:
 
     #configuracion de base de datos
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:71W8BisG3nqdufsIBj06@containers-us-west-153.railway.app:6899/railway"
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/api_flashcards"
     #caa269e6-544a-41d8-8b27-ba3d95a638c1
     #SQLALCHEMY_DATABASE_URI = "postgresql://lffvohgbhqtidc:ba6ffb9ff8c954004cf21fbe0faa1ac1f27832206b5f015e8b8f1a1795c9e448@ec2-54-160-200-167.compute-1.amazonaws.com:5432/d1n7s1vclebbbt"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:KlNBJQjP89xLnr0HhUj5@containers-us-west-162.railway.app:7361/railway"
+    
 
     
 #clases que herada de Config
