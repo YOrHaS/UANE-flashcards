fa6b003fdf20d624f5becd2011116caee33f69a4
YOrHaS <100461126+YOrHaS@users.noreply.github.com>
2022-12-12 18:37:00 -0600

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,11 @@ class Config:
     #configuracion de base de datos
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/api_flashcards"
-    SQLALCHEMY_DATABASE_URI = "postgresql://lffvohgbhqtidc:ba6ffb9ff8c954004cf21fbe0faa1ac1f27832206b5f015e8b8f1a1795c9e448@ec2-54-160-200-167.compute-1.amazonaws.com:5432/d1n7s1vclebbbt"
+    #caa269e6-544a-41d8-8b27-ba3d95a638c1
+    #SQLALCHEMY_DATABASE_URI = "postgresql://lffvohgbhqtidc:ba6ffb9ff8c954004cf21fbe0faa1ac1f27832206b5f015e8b8f1a1795c9e448@ec2-54-160-200-167.compute-1.amazonaws.com:5432/d1n7s1vclebbbt"
 
+    SQLALCHEMY_DATABASE_URI = "postgresql://caa269e6-544a-41d8-8b27-ba3d95a638c1
+    
 #clases que herada de Config
 class ProductionConfig(Config):
     DEBUG = False
