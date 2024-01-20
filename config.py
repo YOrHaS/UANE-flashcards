ff4e72c57848e63554dca6dd267bb41ee793ec43
YOrHaS <100461126+YOrHaS@users.noreply.github.com>
2024-01-19 23:17:05 -0600

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     #configuracion de base de datos
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    SQLALCHEMY_DATABASE_URI = "postgres://postgresql_card_user:8EtuEzdHy16akC2UMXxACVS82wbALVTq@dpg-cmlkm821hbls73ca31d0-a/postgresql_card"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgresql_card_user:8EtuEzdHy16akC2UMXxACVS82wbALVTq@dpg-cmlkm821hbls73ca31d0-a/postgresql_card"
     #postgresql://postgres:71W8BisG3nqdufsIBj06@containers-us-west-153.railway.app:6899/railway"
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/api_flashcards"
     #caa269e6-544a-41d8-8b27-ba3d95a638c1
